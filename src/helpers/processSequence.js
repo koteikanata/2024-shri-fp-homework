e2e066d4103109b4ee2f4ef78de11f3edd86934a
Nataly <65140786+koteikanata@users.noreply.github.com>
2024-07-25 16:57:07 +0300

    feat: add functionality to processSequence.js

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,99 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import Api from '../tools/api';
+import {
+    allPass,
+    andThen,
+    compose,
+    curry,
+    gt,
+    ifElse,
+    length,
+    lt,
+    otherwise,
+    pipe,
+    prop,
+    tap,
+    test,
+} from 'ramda';
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const longerThanTwo = compose(lt(2), length);
+const shorterThanTen = compose(gt(10), length);
+const isNumber = test(/^\d+(\.\d+)?$/);
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const isValid = allPass([longerThanTwo, shorterThanTen, isNumber]);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
+const strToNumber = compose(Math.round, Number);
+const numToStr = String;
+const getStrLength = length;
+const squaring = (num) => Math.pow(num, 2);
+const getRemainOfDivision = curry((div, num) => num % div);
+const getResult = prop('result');
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+/**
+ *
+ * @param {Object} object
+ * @param {string} object.value
+ * @param {Function} object.writeLog
+ * @param {Function} object.handleSuccess
+ * @param {Function} object.handleError
+ *
+ */
+const processSequence = ({
+    value,
+    writeLog,
+    handleSuccess,
+    handleError,
+}) => {
+    const logCurrentValue = tap(writeLog);
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+    const handleApiRejection = (error) => {
+        handleError('rejected response');
+        return Promise.reject(error);
+    };
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+    const handleAnimalResponse = pipe(getResult, handleSuccess);
+
+    const requestToAnimal = pipe(
+        getRemainOfDivision(3),
+        logCurrentValue,
+        (id) => api.get(`https://animals.tech/${id}`, {})
+    );
+
+    const handleNumberResponse = pipe(
+        getResult,
+        logCurrentValue,
+        numToStr,
+        getStrLength,
+        logCurrentValue,
+        squaring,
+        logCurrentValue,
+        requestToAnimal,
+        andThen(handleAnimalResponse),
+        otherwise(handleApiRejection)
+    );
+
+    const runNumberConvert = pipe(strToNumber, logCurrentValue, (num) =>
+        api
+            .get('https://api.tech/numbers/base', {
+                number: num,
+                from: 10,
+                to: 2,
+        }),
+        andThen(handleNumberResponse),
+        otherwise(handleApiRejection)
+    );
+
+    const validateAndRun = ifElse(
+        isValid,
+        pipe(logCurrentValue, runNumberConvert),
+        () => handleError('ValidationError'),
+    );
+
+    validateAndRun(value);
+};
 
 export default processSequence;
